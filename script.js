9ea2a11087e52dca21d6a68be06bc3c8469da1bd
NajwaMustaffa <najwamstaffa2@gmail.com>
2022-10-21 21:51:28 +0800

    Popup

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+    let password = document.getElementById("password").value;
+    if (password.length <= 12) {
+      alert("harap maaf, kata laluan anda tidak mengandungi huruf besar")
+    }
+    else {
+      alert("password");
+    }
+  
+  }
